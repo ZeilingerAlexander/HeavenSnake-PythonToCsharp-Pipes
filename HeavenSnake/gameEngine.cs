f095c4409fc587c74020bb545aacd9c9656542d7
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2023-11-14 11:31:29 +0100

    finished the respawn algorithm to find free position for fruit

--- HeavenSnake/gameEngine.cs
+++ HeavenSnake/gameEngine.cs
@@ -5,11 +5,12 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
 namespace HeavenSnake
 {
     internal class GameEngine
     {
+        Random rnd = new Random();
+
         /// <summary>
         /// The Time between each movement action
         /// </summary>
@@ -26,6 +27,7 @@ namespace HeavenSnake
         /// When moving the Parts to the head, make sure head was moved first before initializing the movement recursion
         /// </summary>
         Part Head { get; set; }
+        int Score { get; set; }
         Program.Rotation HeadRotation { get; set; }
         /// <summary>
         /// Field size is the direct width and height of the field + 1 meaning that positions start from 0 and end at x so the snake can always move from 0 to x
@@ -33,13 +35,49 @@ namespace HeavenSnake
         Program.Vector2INT FieldSize { get; set; }
         Program.Vector2INT FruitPos { get; set; }
 
+        /// <summary>
+        /// Gets all the positions availiabel for the fruit to spawn into
+        /// </summary>
+        /// <returns></returns>
+        Program.Vector2INT[] GetAvailiableFruitPositions()
+        {
+            // Get all availiable Positions
+            int TotalPositions = (FieldSize.x+1) * (FieldSize.y+1); // Total positions
+            int FreePositions = TotalPositions - (Parts.Count + 1); // Positions not occupied by parts
+            Program.Vector2INT[] AvailiablePositons = new Program.Vector2INT[FreePositions]; // Will contain all availiable positions
+            int currentIndexPos = 0; // Temporary variable to store the current index of the array where to write a free position to
+            for (int xAxis = 0; xAxis <= FieldSize.x; xAxis++)
+            {
+                for (int yAxis = 0; yAxis <= FieldSize.y; yAxis++)
+                {
+                    if (!Parts.Any(p => p.Position.x == xAxis && p.Position.y == yAxis) &&!(Head.Position.x == xAxis && Head.Position.y == yAxis))
+                    {
+                        // Position is availiable
+                        AvailiablePositons[currentIndexPos] = new Program.Vector2INT() { x = xAxis, y = yAxis };
+                        currentIndexPos++;
+                    }
+                }
+            }
+
+            return AvailiablePositons;
+        }
         void RespawnFruit()
         {
-
+            Program.Vector2INT[] AvailiablePositons = GetAvailiableFruitPositions();
+            // Get a Random Position out of the availiable positions
+            int rndIndex = rnd.Next(0, AvailiablePositons.Length);
+            FruitPos = AvailiablePositons[rndIndex];
         }
         void PrintGame()
         {
 
+        }
+        /// <summary>
+        /// Checks if the Snake head is on the Fruit
+        /// </summary>
+        bool CheckIfEligibleForScore()
+        {
+            if (Head.Position.x == FruitPos.x && Head.Position.y == FruitPos.y) { return true; } return false;
         }
         /// <summary>
         /// Moves the Snake to the current direction
@@ -112,7 +150,6 @@ namespace HeavenSnake
                 UserInput = Console.ReadKey().Key;
             });
             userInputTask.Wait(TimeBetweenMovement);
-
             // UserInput is now either a key they pressed or no name if they didnt press anything in the timespan
             return UserInput;
         }
@@ -151,9 +188,16 @@ namespace HeavenSnake
         }
         void GameLoop()
         {
+            // Print the game then wait for input until time is over, handle that input if there even was any then move the snake and check if elegible for score
             PrintGame();
             ConsoleKey key = RequestInput();
             HandleInput(key);
+            MoveSnake();
+            if (CheckIfEligibleForScore())
+            {
+                Score++;
+                RespawnFruit();
+            }
         }
 
         public GameEngine(Program.Vector2INT FieldSize)
